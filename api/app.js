08c6031f3b2d8b075964a21550ce493da2911569
Butko Denys <Butko.dv@gmail.com>
2024-10-16 13:32:20 +0000

    поместил в контейнер

--- api/app.js
+++ api/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const { NODE_PORT, VITE_BASE_URL } = process.env;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello World! ${NODE_PORT}`);
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(NODE_PORT, () => {
+  console.log(`Server is listening on port  ${NODE_PORT}`);
 });
