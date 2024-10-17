882fbf44ec6ab08bd7fb6fb305ae202f6b9d6d72
Denis Butko <butko.dv@gmail.com>
2024-10-17 23:00:38 +0300

    tailwind добавил

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <>
-      <div>test</div>
-      <p>Click on the Vite and React logos to learn more</p>
+      <h1 className="text-xl font-bold ">Hello world!</h1>
+
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        Button
+      </button>
     </>
   );
 }
