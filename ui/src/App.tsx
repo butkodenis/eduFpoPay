31755b18f794400166b52027999d54a405241689
Butko Denys <Butko.dv@gmail.com>
2024-10-21 06:40:17 +0000

    shadcn/ui добавил

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,13 @@
 import './App.css';
+import { useState } from 'react';
+
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/style.css';
+
+import { Button } from '@/components/ui/button';
 
 function App() {
+  const [selected, setSelected] = useState<Date>();
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -11,6 +18,18 @@ function App() {
         <span className="font-medium">Info alert!</span> Change a few things up
         and try submitting again.
       </div>
+      <Button>Click me</Button>
+      <Button variant="link">Link</Button>
+      <DayPicker
+        mode="single"
+        selected={selected}
+        onSelect={setSelected}
+        footer={
+          selected
+            ? `Selected: ${selected.toLocaleDateString()}`
+            : 'Pick a day.'
+        }
+      />
     </>
   );
 }
