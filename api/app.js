36ed1f7ffb1893c60a9b5631b81d3b309a95ec2d
Butko Denys <Butko.dv@gmail.com>
2024-11-05 12:56:34 +0000

    добавлен ф-я добавления курсов .

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const { NODE_PORT, VITE_BASE_URL } = process.env;
 
 app.use(morgan('combined'));
+app.use(express.json());
 
 //const sequelize = require('./Utilites/dbConfig');
 const syncDatabase = require('./Utilites/sync');
@@ -14,6 +15,8 @@ const syncDatabase = require('./Utilites/sync');
 app.get('/api', (req, res) => {
   res.send(`Hello World! ${NODE_PORT}`);
 });
+
+app.use('/api', require('./Routers/courseRouter'));
 
 app.listen(NODE_PORT, () => {
   console.log(`Server is listening on port  ${NODE_PORT}`);
