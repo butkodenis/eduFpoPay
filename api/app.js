fd8177b5f11cbe018030d6cab48ae9f582434af7
Butko Denys <Butko.dv@gmail.com>
2024-11-06 09:18:27 +0000

    добавлен CORS для обработки запросов с указанного origin

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -8,6 +9,13 @@ const { NODE_PORT, VITE_BASE_URL } = process.env;
 
 app.use(morgan('combined'));
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://127.0.0.1:5173', // Укажите origin, откуда вы делаете запросы
+    credentials: true, // Включите передачу cookies или других учетных данных
+  })
+);
 
 //const sequelize = require('./Utilites/dbConfig');
 const syncDatabase = require('./Utilites/sync');
