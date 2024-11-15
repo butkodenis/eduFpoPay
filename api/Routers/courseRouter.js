3afda0ddd69677782d2465e07571309f29a1a1af
Butko Denys <Butko.dv@gmail.com>
2024-11-15 08:33:36 +0000

    добавлено удаление и обновление курсов с соответствующими маршрутами в контроллере курсов

--- api/Routers/courseRouter.js
+++ api/Routers/courseRouter.js
@@ -3,5 +3,7 @@ const courseController = require('../Controllers/courseController');
 
 router.post('/courses/create', courseController.createCourse);
 router.get('/courses/all', courseController.getCourses);
+router.delete('/courses/delete/:id', courseController.deleteCourse);
+router.put('/courses/update/:id', courseController.updateCourse);
 
 module.exports = router;
